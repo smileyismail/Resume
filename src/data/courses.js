dcc95dc0d4145c3381bbb7135142dcaaf352c575
Smiley <smileyismail67@gmail.com>
2023-08-24 00:14:15 +0530

    asd

--- src/data/courses.js
+++ src/data/courses.js
@@ -1,12 +1,12 @@
 export const courses = [
   {
-    id: 3,
-    title: "React Native - The Practical Guide.",
+    id: 2,
+    title: "React.js - The Complete Guide.",
     platform: "Udemy",
   },
   {
-    id: 2,
-    title: "React.js - The Complete Guide.",
+    id: 3,
+    title: "React Native - The Practical Guide.",
     platform: "Udemy",
   },
   {
