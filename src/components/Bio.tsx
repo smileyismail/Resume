b23e3522aae0ecc85655d34544db744565855125
Smiley <smileyismail67@gmail.com>
2023-10-14 02:56:51 +0530

    sad

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,14 +1,16 @@
-import { bio } from "../data/bio";
+import { developerBio } from "../data/bio";
 
 const Bio = () => {
   return (
     <section className="h-full flex flex-col justify-evenly">
       <h1 className="text-3xl font-bold text-black">Syed Ismailuddin</h1>
 
-      <h2 className="text-xl font-semibold text-accent">{bio.title}</h2>
+      <h2 className="text-xl font-semibold text-accent">
+        {developerBio.title}
+      </h2>
 
       <div className="text-sm text-justify flex flex-col justify-evenly text-neutral-900">
-        {bio.bio.map((point, index) => (
+        {developerBio.bio.map((point, index) => (
           <p key={index} className="indent-10">
             {point}
           </p>
